c0d7e321e4bc2b735337c0e61e1d8b5403e28465
ttoghere <59641232+ttoghere@users.noreply.github.com>
2022-05-07 20:44:37 +0300

    hesap ekranı

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,11 @@
+import 'package:amazonclone/model/user_details.dart';
 import 'package:amazonclone/utils/constants.dart';
 import 'package:amazonclone/widgets/banner_ad_widget.dart';
 import 'package:amazonclone/widgets/categories_horizontal_list_view_bar.dart';
 import 'package:amazonclone/widgets/products_showcase_list_view.dart';
 import 'package:amazonclone/widgets/search_bar_widget.dart';
 import 'package:amazonclone/widgets/simple_product_widget.dart';
+import 'package:amazonclone/widgets/user_details.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -14,33 +16,65 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  ScrollController scrollController = ScrollController();
+  double offset = 0;
+  @override
+  void initState() {
+    super.initState();
+    scrollController.addListener(() {
+      setState(() {
+        offset = scrollController.position.pixels;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: SearchBarWidget(
-        isReadOnly: true,
-        hasBackButton: false,
-      ),
-      body: SingleChildScrollView(
-        child: Column(
+    return SafeArea(
+      child: Scaffold(
+        appBar: SearchBarWidget(
+          isReadOnly: true,
+          hasBackButton: false,
+        ),
+        body: Stack(
           children: [
-            CategoriesHorizontalListViewBar(),
-            BannerAdWidget(),
-            ProductsShowcaseListView(
-              title: "Upto 70% off",
-              children: testChildren,
+            SingleChildScrollView(
+              controller: scrollController,
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: kAppBarHeight / 2,
+                  ),
+                  CategoriesHorizontalListViewBar(),
+                  BannerAdWidget(),
+                  ProductsShowcaseListView(
+                    title: "Upto 70% off",
+                    children: testChildren,
+                  ),
+                  ProductsShowcaseListView(
+                    title: "Upto 60% off",
+                    children: testChildren,
+                  ),
+                  ProductsShowcaseListView(
+                    title: "Upto 50% off",
+                    children: testChildren,
+                  ),
+                  ProductsShowcaseListView(
+                    title: "Explore",
+                    children: testChildren,
+                  ),
+                ],
+              ),
             ),
-            ProductsShowcaseListView(
-              title: "Upto 60% off",
-              children: testChildren,
-            ),
-            ProductsShowcaseListView(
-              title: "Upto 50% off",
-              children: testChildren,
-            ),
-            ProductsShowcaseListView(
-              title: "Explore",
-              children: testChildren,
+            UserDetailsBar(
+              offset: offset,
+              userDetailsModel: UserDetailsModel(address: "Ev", name: "Ayşe"),
             ),
           ],
         ),
--- lib/widgets/user_details.dart
+++ lib/widgets/user_details.dart
@@ -1,1 +1,59 @@
 import 'package:flutter/material.dart';
+
+import 'package:amazonclone/model/user_details.dart';
+import 'package:amazonclone/utils/colors.dart';
+import 'package:amazonclone/utils/constants.dart';
+import 'package:amazonclone/utils/utils.dart';
+
+class UserDetailsBar extends StatelessWidget {
+  final double offset;
+  final UserDetailsModel userDetailsModel;
+  const UserDetailsBar({
+    Key? key,
+    required this.offset,
+    required this.userDetailsModel,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size screenSize = Utils().getScreenSize();
+    return Positioned(
+      top: -offset / 3,
+      child: Container(
+        width: screenSize.width,
+        height: kAppBarHeight / 2,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: lightBackgroundaGradient,
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 3, horizontal: 20),
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 8.0),
+                child: Icon(
+                  Icons.location_on_outlined,
+                  color: Colors.grey[900],
+                ),
+              ),
+              SizedBox(
+                width: screenSize.width * 0.7,
+                child: Text(
+                  "Deliver to ${userDetailsModel.name} - ${userDetailsModel.address}",
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                    color: Colors.grey[900],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
